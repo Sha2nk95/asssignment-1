51dfa59627c746ca2262f5618f09e116b9bc29ca
Sha2nk95 <shashankg54@gmail.com>
2025-05-30 14:11:26 +0530

    Add files via upload

--- assignment 1_1.py
+++ assignment 1_1.py
@@ -1,1 +1,25 @@
+first_number = 5
+second_number = 10
 
+print("---our numbers---")
+print(f"first number: {first_number}")
+print(f"second number: {second_number}")
+print("---------\n")
+
+# 1. Addition (+)
+sum_result = first_number + second_number
+print(f"Addition: {first_number} + {second_number} = {sum_result}")
+
+# 2. Subtraction (-)
+sub_result_1 = first_number - second_number
+sub_result_2 = second_number - first_number
+print(f"Subtraction: {first_number} - {second_number} = {sub_result_1}")
+
+
+# 3. Multiplication (*)
+mul_result = first_number * second_number
+print(f"Multiplication: {first_number} * {second_number} = {mul_result}")
+
+# 4. Division (/)
+div_result_1 = first_number / second_number
+print(f"Division: {first_number} / {second_number} = {div_result_1}")
